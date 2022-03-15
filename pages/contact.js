49e32efba115f44419ba99382e76d64f0ab0a358
nafsuki <48878455+Nafsuki@users.noreply.github.com>
2022-03-15 22:02:11 +0100

    input type hidden added

--- pages/contact.js
+++ pages/contact.js
@@ -4,8 +4,34 @@ import Layout from "../components/Layout";
 export default function contact() {
   return (
     <Layout>
-      <div>contact</div>
+      <h1>contact</h1>
+      {/* <form name="contact" method="POST" className="flex flex-col p-5 space-y-3 text-lg border rounded-lg">
+      <input type="hidden" name="form-name" value="contact" />
+      <p>
+<input type="text" name="firstname" id="firstname" />
+        <label htmlFor="yourname">
+          Your Name:
+        </label> <br />
+        <input type="text" name="name" id="yourname" />
+      </p>
+      <p>
+        <label htmlFor="youremail">
+          Your Email:
+        </label> <br />
+        <input type="email" name="email" id="youremail" />
+      </p>
+      <p>
+        <label htmlFor="yourmessage">
+          Message:
+        </label> <br />
+        <textarea name="message" id="yourmessage"></textarea>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form> */}
       <form className="flex flex-col p-5 space-y-3 text-lg border rounded-lg" name='contact' method='POST' data-netlify='true'>
+        <input type='hidden' name="form-name" value='contact' />
         <p>
           <label htmlFor="name">Name</label><br/>
           <input className='w-[500px] rounded' type="text" id='name' name='name' />

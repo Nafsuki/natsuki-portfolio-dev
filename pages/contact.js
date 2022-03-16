3046a3c4ec083ea9ef686ee82e320ac628261131
nafsuki <48878455+Nafsuki@users.noreply.github.com>
2022-03-16 10:35:24 +0100

    input required attribute added

--- pages/contact.js
+++ pages/contact.js
@@ -4,41 +4,16 @@ import Layout from "../components/Layout";
 export default function contact() {
   return (
     <Layout>
-      <h1>contact</h1>
-      {/* <form name="contact" method="POST" className="flex flex-col p-5 space-y-3 text-lg border rounded-lg">
-      <input type="hidden" name="form-name" value="contact" />
-      <p>
-<input type="text" name="firstname" id="firstname" />
-        <label htmlFor="yourname">
-          Your Name:
-        </label> <br />
-        <input type="text" name="name" id="yourname" />
-      </p>
-      <p>
-        <label htmlFor="youremail">
-          Your Email:
-        </label> <br />
-        <input type="email" name="email" id="youremail" />
-      </p>
-      <p>
-        <label htmlFor="yourmessage">
-          Message:
-        </label> <br />
-        <textarea name="message" id="yourmessage"></textarea>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form> */}
+      <h1 className="text-lg font-bold">Contact Form</h1>
       <form className="flex flex-col p-5 space-y-3 text-lg border rounded-lg" name='contact' method='POST' data-netlify='true' netlify-honeypot="bot-field">
         <input type='hidden' name="form-name" value='contact' />
         <p>
           <label htmlFor="name">Name</label><br/>
-          <input className='w-[500px] rounded' type="text" id='name' name='name' />
+          <input required className='w-[500px] rounded' type="text" id='name' name='name' />
         </p>
         <p>
           <label htmlFor="email">Email</label><br/>
-          <input className='w-[500px] rounded' type="text" id='email' name='email' />
+          <input required className='w-[500px] rounded' type="text" id='email' name='email' />
         </p>
         <p>
           <label htmlFor="message">Message</label><br/>

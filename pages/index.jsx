32649ada56700fd02fd80a72695e647ce69f577d
nafsuki <48878455+Nafsuki@users.noreply.github.com>
2022-04-22 12:06:30 +0200

    sns icons & links added

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
 
 import Layout from '../components/Layout';
 
@@ -22,6 +23,15 @@ export default function Home() {
 					<div className='m-2 text-sm md:text-left md:text-lg'>
 						<h6>Frontend Develper</h6>
 						<h6>Located in Hamburg, Germany</h6>
+						{/* SNS Icons */}
+						<div className='flex justify-center mt-4 space-x-3 md:justify-start'>
+							<Link href='https://www.linkedin.com/in/nafsuki/' passHref>
+								<FaLinkedin className='text-4xl text-[#1666C2] cursor-pointer hover:opacity-70 transition-opacity' />
+							</Link>
+							<Link href='https://github.com/Nafsuki' passHref>
+								<FaGithub className='text-4xl transition-opacity cursor-pointer hover:opacity-70' />
+							</Link>
+						</div>
 					</div>
 					<Link href='/projects' passHref>
 						<button className='dark:bg-indigo-500 dark:hover:bg-indigo-200 dark:hover:text-indigo-900 dark:border-blue-100 text-sm md:text-lg shadow-lg hover:bg-pink-700 hover:text-white transition-colors font-bold bg-pink-50 border-pink-700 border w-[170px] h-[45px] my-5 mx-auto md:mx-0 md:w-[200px] md:h-[50px] md:mt-5 rounded-full'>
